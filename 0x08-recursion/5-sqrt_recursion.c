a5779413442e76b975899afa7f576c1326273789
ethioenud <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@github.com>
2023-10-01 09:08:03 -0700

    project

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+
+/**
+ * check - checks for the square root
+ * @a:int
+ * @b:int
+ *
+ * Return: int
+ */
+
+int check(int a, int b)
+
+{
+
+if (a * a == b)
+
+return (a);
+
+if (a * a > b)
+
+return (-1);
+
+return (check(a + 1, b));
+}
