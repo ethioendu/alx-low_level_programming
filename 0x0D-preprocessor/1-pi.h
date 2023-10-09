1127ea42a9e9028f3cab46d81f9d7bd031b6fa92
ethioenud <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@github.com>
2023-10-09 08:00:03 -0700

    preprocessor

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,46 @@
+#ifndef _PI_H_
+#define _PI_H_
+
+#define PI 3.14159265359
+
+#endif#ifndef _PI_H_
+#define _PI_H_
+
+#define PI 3.14159265359
+
+#endif#ifndef _PI_H_
+#define _PI_H_
+
+#define PI 3.14159265359
+
+#endif#ifndef _PI_H_
+#define _PI_H_
+
+#define PI 3.14159265359
+
+#endif#ifndef _PI_H_
+#define _PI_H_
+
+#define PI 3.14159265359
+
+#endif#ifndef _PI_H_
+#define _PI_H_
+
+#define PI 3.14159265359
+
+#endif#ifndef _PI_H_
+#define _PI_H_
+
+#define PI 3.14159265359
+
+#endif#ifndef _PI_H_
+#define _PI_H_
+
+#define PI 3.14159265359
+
+#endif#ifndef _PI_H_
+#define _PI_H_
+
+#define PI 3.14159265359
+
+#endif
