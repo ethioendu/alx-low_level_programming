0515f854cdbe77a4ef04830d197f4b20d03d7a96
ethioenud <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@github.com>
2023-10-19 15:06:10 -0700

    project

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * list_len - a function that returns the number of elements in a linked list_t list.
+ * list_len - returns the number of elements in a linked list
  * @h: name of the list
  * Return: the number of nodes.
  */
